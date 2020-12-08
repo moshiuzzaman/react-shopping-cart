4ea2d5f6f5b2cfe75413629ed55ce7f2d5e1a594
moshiuzzaman <md.shozon041@gmail.com>
2020-12-08 18:54:20 +0600

    checkout section complete

--- src/Component/Main/Main.js
+++ src/Component/Main/Main.js
@@ -10,38 +10,42 @@ import { Box } from '@material-ui/core';
 export default function Main() {
   const [data, setData] = useState({
     products: Products,
-    cartItems: [],
+    cartItems: localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : [],
     size: '',
     sort: '',
   })
-  const AddToCart=(product)=>{
-    const cartItems =data.cartItems.slice();
-    let alradyAdded=false;
-    cartItems.map(pd=>{
-      if(pd._id===product._id){
+  const AddToCart = (product) => {
+    const cartItems = data.cartItems.slice();
+    let alradyAdded = false;
+    cartItems.map(pd => {
+      if (pd._id === product._id) {
         pd.count++;
-        alradyAdded=true;
+        alradyAdded = true;
       }
-      return setData({...data, cartItems})
+      localStorage.setItem("cartItems", JSON.stringify(cartItems))
+      return setData({ ...data, cartItems })
     })
-    if(!alradyAdded){
-      cartItems.push({...product, count:1})
-    setData({...data, cartItems})
-    }  
-}
-const removeFromCart=(id)=>{
- const newCart= data.cartItems.filter(ci=>ci._id!==id)
- setData({...data, cartItems:newCart})
-}
+    if (!alradyAdded) {
+      cartItems.push({ ...product, count: 1 })
+      localStorage.setItem("cartItems", JSON.stringify(cartItems))
+      setData({ ...data, cartItems })
+    }
+  }
+  const removeFromCart = (id) => {
+    const newCart = data.cartItems.filter(ci => ci._id !== id)
+    setData({ ...data, cartItems: newCart })
+    localStorage.setItem("cartItems", JSON.stringify(newCart))
+  } 
+   
   return (
     <Box m={3}>
       <Grid container spacing={3}>
-        <Grid item md={8}>
+        <Grid item md={9}>
 
           <Product AddToCart={AddToCart} data={data} setData={setData} />
         </Grid>
-        <Grid item md={4}>
-          <Cart removeFromCart={removeFromCart} data={data} />
+        <Grid item md={3}>
+          <Cart removeFromCart={removeFromCart} setData={setData} data={data} />
         </Grid>
       </Grid>
     </Box>
